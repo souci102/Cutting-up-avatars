4d95da6a301848cc55176922cee01343dd61d5f4
Dmitry Lukanin <dmitry.lukanin@ext-atom.team>
2025-09-02 13:31:43 +0300

    CMYK to RGB

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ rotated_image.save("rotated.png")
 
 print(image.mode)
 print(image.width)
-print(image.height)
+print(image.height, end="\n\n")
+
+image1=Image.open("lenna.jpg")
+print(image1.mode)
+cmyk_image = image.convert("RGB")
+print(cmyk_image.mode)
+print(image1.mode, end="\n\n")
