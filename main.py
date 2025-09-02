b3a754ef3acaded6235cbf3a6db815ea071864b5
Dmitry Lukanin <dmitry.lukanin@ext-atom.team>
2025-09-02 13:14:46 +0300

    width-height

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from PIL import Image
 image = Image.open("example.png")
 rotated_image=image.rotate(45)
 rotated_image.save("rotated.png")
+
+print(image.mode)
+print(image.width)
+print(image.height)
